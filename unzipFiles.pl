5b2a0b6a87b136987bda250faab797689c96abd1
Holly Arnold <Holly.K.Arnold@gmail.com>
2021-03-24 08:46:06 -0700

    file zip .pl

---
+++ unzipFiles.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+# unzipFiles.pl
+# PURPOSE: Zip Files in a directory
+#          
+# INPUTS: 
+#  -i: inDir - an input directory containing files with which to unzip
+# OUTPUTS:
+#  zips all files in a directory
+
+use warnings;
+use strict;
+use Getopt::Long;
+
+my $inDir;
+
+GetOptions(
+    "i:s" => \$inDir,
+    );
+
+opendir(DH, $inDir) or die ("renameFiles.pl: Can't open input directory at ${inDir}. Please check the -i option\n");
+my @files = readdir(DH);
+closedir(DH);
+
+foreach my $file (@files){
+
+    my $cmd = "gunzip ${inDir}${file}";
+    print $cmd . "\n";
+    `$cmd`;
+
+}
